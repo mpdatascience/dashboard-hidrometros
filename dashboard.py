9fbccc5cf75438768cf4de865a0135dbd2c2fb2f
Marcos Paulo <marcosndbtech@gmail.com>
2025-02-18 06:16:45 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -50,7 +50,7 @@ data_menor_consumo = df_menor_consumo["Data"].values[0] if not df_menor_consumo.
 try:
     xls = pd.ExcelFile(caminho_arquivo)
     if "Atual" in xls.sheet_names:
-        df_atual = pd.read_excel(caminho_arquivo, sheet_name="Fev - 2025", usecols="D", nrows=0)
+        df_atual = pd.read_excel(caminho_arquivo, sheet_name="Atual", usecols="B", nrows=1)
         consumo_mes_atual = df_atual.iloc[0, 0] if not df_atual.empty and pd.notna(df_atual.iloc[0, 0]) else "Dados indisponíveis"
     else:
         consumo_mes_atual = "Planilha 'Atual' não encontrada"
