8521f0775dcc8626e8e70fa9676fa03b7ace3a6f
Marcos Paulo <marcosndbtech@gmail.com>
2025-02-20 06:18:16 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta
 from PIL import Image
 
 # Definir caminho do arquivo
+st.image("natura_logo.png", width=200)
 st.set_page_config(page_title="Dashboard Hidrometros", layout="wide")
 caminho_arquivo = os.path.join(os.path.dirname(__file__), "LEITURA DE HIDROMETROS.xlsx")
 
